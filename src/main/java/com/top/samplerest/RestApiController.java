20424507951f83ec0ed920b7c0b7dd6dd055cd31
danilatyukov <atukovdanil69@gmail.com>
2024-09-18 01:19:12 +0700

    "гет кофе, пост кофе, пут кофе, делит кофе

--- src/main/java/com/top/samplerest/RestApiController.java
+++ src/main/java/com/top/samplerest/RestApiController.java
@@ -1,2 +1,51 @@
-package com.top.samplerest;public class RestApiController {
+package com.top.samplerest;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class RestApiController {
+    private List<Coffee> coffees = new ArrayList<>();
+
+    public RestApiController(){
+        coffees.addAll(List.of(
+            new Coffee("Колумб"),
+                new Coffee("Мексиканск")
+        ));
+    }
+
+    //@GetMapping("/coffees")
+    @RequestMapping(value = "/coffees", method = RequestMethod.GET)
+    Iterable<Coffee> getCoffees(){
+        return coffees;
+    }
+
+    @PostMapping("/postCoffee")
+    String postCoffee(@RequestBody Coffee coffee){
+        coffees.add(coffee);
+
+        return "Успех";
+    }
+
+    @PutMapping("/coffees/{id}")
+    String putCoffee(@PathVariable String id, @RequestBody Coffee coffee) {
+        int coffeeIndex = -1;
+
+        coffees.add(coffee);
+
+        return "успех";
+    }
+
+    @DeleteMapping("/coffees/{id}")
+    void deleteCoffee(@PathVariable String id){
+        System.out.println("coffees size "+coffees.size()+ " id = " + id);
+
+        coffees.clear();
+        System.out.println("coffees size "+coffees.size());
+    }
+
 }
+
+
