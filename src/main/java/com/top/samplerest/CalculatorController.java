610b8228613ae131849b0c80209380f0f5a17d24
danilatyukov <atukovdanil69@gmail.com>
2024-09-18 01:33:18 +0700

    заставить работь калькулятор

---
+++ src/main/java/com/top/samplerest/CalculatorController.java
@@ -0,0 +1,26 @@
+package com.top.samplerest;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CalculatorController {
+    @PostMapping("/sumOf/")
+    Integer sumOf(@RequestBody TwoNumbers twoNumbers){
+        System.out.println(twoNumbers);
+
+        return Integer.parseInt(twoNumbers.numA) +
+                Integer.parseInt(twoNumbers.numB);
+    }
+}
+
+class TwoNumbers{
+    String numA;
+    String numB;
+
+    @Override
+    public String toString() {
+        return "numA = " + numA +", numB = "+ numB;
+    }
+}
